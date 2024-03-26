67fb2d3e4fda3291239b848c45aa392fd94b758e
caglenoah <caglenoah@gmail.com>
2024-03-26 17:32:03 -0600

    learned to declare zero values source =  https://go.dev/tour/basics/1

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
-
+var smsSendingLimit int
+var costPerSMS float64
+var hasPermission bool
+var username string 
  
-    fmt.Printf("%v %.2f %v %q/n", smsSendingLimit, costPerSMS, hasPermission, unsername)
+    fmt.Printf("%v %.2f %v %q/n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
 
