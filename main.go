51c90f0d7ddba0d8720b43800babfb66440e23d6
caglenoah <caglenoah@gmail.com>
2024-03-26 17:27:29 -0600

    challenge7..or something lol

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    var senderName = "Bren"
-    var recipient = "Adrian"
-    var message = "The hole, Adrian. You have to boof the hole!"
 
-    fmt.Printf("%s to %s: %s/n", senderName, recipient, message)
-
+ 
+    fmt.Printf("%v %.2f %v %q/n", smsSendingLimit, costPerSMS, hasPermission, unsername)
 }
+
