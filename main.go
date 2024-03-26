5f90c92ea26c88cd660681d9580deac9c69bbcd5
caglenoah <caglenoah@gmail.com>
2024-03-26 17:03:51 -0600

    11. changed the variable type from an intiger to a string. 12. Added quations to the beginning of the strings value. 13. changed the variable name to match the name in the command line.

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
-    var username string = "presidentBilly"
-    var password string = "12345"
-    // var password int = 12345
+var startup string = "Textio SMS service booting up..."
+var message string = "Sending test Message"
+var confirmation string = "Message sent!"
 
-    fmt.Println("Authorization: Basic", username+":"+password)
+
+    // var startup int = "Textio SMS service booting up..."
+    // var message string = Sending test Message"
+    // var confirm string = "Message sent!"
+
+    fmt.Println(startup)
+    fmt.Println(message)
+    fmt.Println(confirmation)
 }
