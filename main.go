07fa53d370cd08e2ab7798bdfdab6a09bcfe4472
caglenoah <caglenoah@gmail.com>
2024-03-26 14:08:00 -0600

    first debug, identified error on line 15, and corrected.

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
+    messagesFromHelga := []string{
+        "Have ya seen me biceps lately",
+        "I can throw a log thrice the distance of half of what you can",
+        "I'm sorry I'm like this please respond",
+        "or not I'll just kick you ass in the town square next time I see you!",
+    }
+numMessages := float64(len(messagesFromHelga))
+costPerMessage := .02
+
+// totalCost := costPerMessage + numMessages
+
+totalCost := costPerMessage * numMessages
+
+fmt.Printf("Helga spent %.2f on text messages today/n", totalCost)
 }
