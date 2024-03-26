d0b55076ebb2316fb028eac3bfb9b03b22caef86
caglenoah <caglenoah@gmail.com>
2024-03-26 16:46:34 -0600

    cannot concatonate different types, go uses strong and static typing. Identified the error on line 8. Changed the type to string

--- main.go
+++ main.go
@@ -3,18 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    messagesFromHelga := []string{
-        "Have ya seen me biceps lately",
-        "I can throw a log thrice the distance of half of what you can",
-        "I'm sorry I'm like this please respond",
-        "or not I'll just kick you ass in the town square next time I see you!",
-    }
-numMessages := float64(len(messagesFromHelga))
-costPerMessage := .02
+    var username string = "presidentBilly"
+    var password string = "12345"
+    // var password int = 12345
 
-// totalCost := costPerMessage + numMessages
-
-totalCost := costPerMessage * numMessages
-
-fmt.Printf("Helga spent %.2f on text messages today/n", totalCost)
+    fmt.Println("Authorization: Basic", username+":"+password)
 }
