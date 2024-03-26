baefdb5a23adb4fd559a22ba8a6c954497e08b86
caglenoah <caglenoah@gmail.com>
2024-03-26 17:15:10 -0600

    added var, originally i added the type aswell and got the same result. That is because In Go, the %s and %v are formatting verbs used with the printf family of functions. This is my source https://www.quora.com/In-Go-when-would-I-use-s-for-string-formatting-I-have-read-the-doc-and-experimented-with-many-values-but-I-still-don-t-understand-How-is-it-different-from-v

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    senderName = "Bren"
-    recipient = "Adrian"
-    message = "The hole, Adrian. You have to boof the hole!"
+    var senderName = "Bren"
+    var recipient = "Adrian"
+    var message = "The hole, Adrian. You have to boof the hole!"
 
     fmt.Printf("%s to %s: %s/n", senderName, recipient, message)
 
