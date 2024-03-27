07ccbf7e4ea9635e727f962e68295f44f44c7626
caglenoah <caglenoah@gmail.com>
2024-03-26 22:35:07 -0600

    new challenge

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    const secondsInMinute = 60
-    const minutesInHour = 60
-    const secondsInHour = secondsInMinute * minutesInHour
+    const name = "Saul Goodman"
+    const openRate = 30.5
 
-    fmt.Println("number of seconds in an hour:", secondsInHour)
-
+    fmt.Print(msg)
 }
