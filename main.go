9c09e7dc768ad08a6777e0e4b2eec7af80dbd9c9
caglenoah <caglenoah@gmail.com>
2024-03-27 16:29:37 -0600

    new challenge

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-    messageLen := 10
-    maxMessageLen := 20
-    fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
+    fname := "Dalinar"
+    lname := "Kholin"
+    age := 45
+    messageRate := 0.5
+    isSubscribed := false
+    message := "Sometimes hypocrite is nothing more han a man in the process of changing."
 
-    if messageLen < maxMessageLen {
-        fmt.Println("Message sent")
-    } else {
-        fmt.Println("Message not sent")
-    }
+    userLog := fmt.Sprintf("Name: ? ?, Age: ?, Rate: ?, isSubscribed: ?, Message: ?")
+
+    fmt.Println(userLog)
 }
