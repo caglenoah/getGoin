478975772321fb5222cc1c26902bd3b2a700f410
caglenoah <caglenoah@gmail.com>
2024-03-26 21:53:44 -0600

    i learnd about walruses lol. You can only infer within a function. Global variable must use declarations.

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+messageStart := "Happy Birthday! you are now"
+age := 21 
+messageEnd := "years old!"
+
 
     fmt.Println(messageStart, age, messageEnd)
 }
