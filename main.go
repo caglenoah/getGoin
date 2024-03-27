4324700b25fbf170eb18258a8c9cc76b0ef8337c
caglenoah <caglenoah@gmail.com>
2024-03-27 17:13:43 -0600

    notice the operaters used to simplify code -=

--- main.go
+++ main.go
@@ -11,5 +11,25 @@ func main() {
     var discountRate float64 = 0.10
     var finalCost float64
 
+
+//set finalCost to bulkMessageCost
+finalCost = bulkMessageCost
+
+if isPremiumUser == true {
+    finalCost -= bulkMessageCost * discountRate
+}
+
+
+if accountBalance > finalCost {
+    fmt.Println(purchaseSuccessMessage)
+} else {
+    fmt.Println(insufficientFundMesssage)
+}
+accountBalance = accountBalance - finalCost
+//if user is premium, discount should apply to final cost
+//if there is more money in account balance print purchase succcess message
+//if there is less money in accound balance print the puchase failed message
+
+
     fmt.Println("Account balance:", accountBalance)
 }
