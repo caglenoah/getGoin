09aa667e0e93d5c5d58d1b036a77763f8da30596
caglenoah <caglenoah@gmail.com>
2024-03-27 17:20:36 -0600

    new challenge

--- main.go
+++ main.go
@@ -2,34 +2,16 @@ package main
 
 import "fmt"
 
+func concat(s1, s2) string {
+    return s1 + s2
+}
+
 func main() {
-    var insufficientFundMesssage string = "Purchase failed. Insuffficient funds."
-    var purchaseSuccessMessage string = "Purchase succesful."
-    var accountBalance float64 = 100.0
-    var bulkMessageCost float64 = 75.0 
-    var isPremiumUser bool = true
-    var discountRate float64 = 0.10
-    var finalCost float64
-
-
-//set finalCost to bulkMessageCost
-finalCost = bulkMessageCost
-
-if isPremiumUser == true {
-    finalCost -= bulkMessageCost * discountRate
+    test("Lane", " happy birthday!")
+    test("Elon,", "hope that tesla thing works out")
+    test("Go", " is fantastic")
 }
 
-
-if accountBalance > finalCost {
-    fmt.Println(purchaseSuccessMessage)
-} else {
-    fmt.Println(insufficientFundMesssage)
-}
-accountBalance = accountBalance - finalCost
-//if user is premium, discount should apply to final cost
-//if there is more money in account balance print purchase succcess message
-//if there is less money in accound balance print the puchase failed message
-
-
-    fmt.Println("Account balance:", accountBalance)
+func test(s1 string, s2 string) {
+    fmt.Println(concat(s1, s2))
 }
