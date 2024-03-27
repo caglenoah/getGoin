a932cd6a5b5912173ffe62c8639d481faa36f819
caglenoah <caglenoah@gmail.com>
2024-03-27 16:23:51 -0600

    if statements in go do not use parenthesis

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
     maxMessageLen := 20
     fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
 
-    if messageLen > MaxMessageLen {
+    if messageLen < maxMessageLen {
         fmt.Println("Message sent")
     } else {
         fmt.Println("Message not sent")
