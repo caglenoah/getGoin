50adf13b5e2b27348c8b60db643b8b663946538b
caglenoah <caglenoah@gmail.com>
2024-03-27 16:42:04 -0600

    nice, hard to remember those things just referenced passed commits. Nice documentation bud :)

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
     isSubscribed := false
     message := "Sometimes hypocrite is nothing more han a man in the process of changing."
 
-    userLog := fmt.Sprintf("Name: ? ?, Age: ?, Rate: ?, isSubscribed: ?, Message: ?")
+    userLog := fmt.Sprintf("Name: %s %s, Age: %d, Rate: %.1f, isSubscribed: %t, Message: %s\n", fname, lname, age, messageRate, isSubscribed, message)
 
     fmt.Println(userLog)
 }
